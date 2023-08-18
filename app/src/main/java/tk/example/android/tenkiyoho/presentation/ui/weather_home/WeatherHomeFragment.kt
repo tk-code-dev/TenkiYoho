7f93251e4459ff552653fdf1a2a0d882e8057157
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2023-08-18 10:13:58 +0900

    Add error message dialog for testing

--- app/src/main/java/tk/example/android/tenkiyoho/presentation/ui/weather_home/WeatherHomeFragment.kt
+++ app/src/main/java/tk/example/android/tenkiyoho/presentation/ui/weather_home/WeatherHomeFragment.kt
@@ -1,9 +1,11 @@
 package tk.example.android.tenkiyoho.presentation.ui.weather_home
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import tk.example.android.tenkiyoho.R
 import tk.example.android.tenkiyoho.databinding.FragmentWeatherHomeBinding
@@ -24,11 +26,25 @@ class WeatherHomeFragment : BaseFragment() {
 
     override fun subscribeUi() {
         binding?.buttonToDetails?.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_home_to_details,
-                null
-            )
+            showDialog()
+//            findNavController().navigate(
+//                R.id.action_home_to_details,
+//                null
+//            )
         }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.dialog_error_message)
+
+        val closeButton = dialog.findViewById<Button>(R.id.dialogButton)
+        closeButton.setOnClickListener {
+            dialog.dismiss() // Close the dialog when the button is clicked
+        }
+
+        dialog.show()
+
     }
 
 }
